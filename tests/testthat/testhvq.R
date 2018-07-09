0b35b806fc9e7ae8e38041c49566f61c99d9fed7
Dave.Kirankumar <dave.kirankumar@mu-sigma.com>
2018-07-09 19:01:56 +0530

    Version number changed

--- tests/testthat/testhvq.R
+++ tests/testthat/testhvq.R
@@ -17,7 +17,7 @@ test_that("getCentroids give correct results for L1_Norm and mean",{
   
   expect_equal(hvt.results$summary[,"Quant.Error"],c(1.679058,2.106304,1.937505),1e-5)
   
-  expect_equal(hvt.results$compression_summary[["Percent_of_cells_below_Quantization_Error_Threshold"]],0)
+  expect_equal(hvt.results$compression_summary[["percentOfCellsBelowQuantizationErrorThreshold"]],0)
   
 })
 
@@ -32,7 +32,7 @@ test_that("getCentroids give correct results for L2_Norm and mean",{
   
   expect_equal(hvt.results$summary[,"Quant.Error"],c(0.9628496,1.2567811,1.1542245),1e-5)
   
-  expect_equal(hvt.results$compression_summary[["Percent_of_cells_below_Quantization_Error_Threshold"]],0)  
+  expect_equal(hvt.results$compression_summary[["percentOfCellsBelowQuantizationErrorThreshold"]],0)  
 })
 
 test_that("getCentroids give correct results for L1_Norm and max",{
@@ -46,7 +46,7 @@ test_that("getCentroids give correct results for L1_Norm and max",{
   
   expect_equal(hvt.results$summary[,"Quant.Error"],c(2.507375,4.059012,3.396253),1e-5)
   
-  expect_equal(hvt.results$compression_summary[["Percent_of_cells_below_Quantization_Error_Threshold"]],0)  
+  expect_equal(hvt.results$compression_summary[["percentOfCellsBelowQuantizationErrorThreshold"]],0)  
 })
 
 test_that("getCentroids give correct results for L2_Norm and max",{
@@ -59,5 +59,5 @@ test_that("getCentroids give correct results for L2_Norm and max",{
   
   expect_equal(hvt.results$summary[,"Quant.Error"],c(1.410263,2.467309,2.288882),1e-5)
   
-  expect_equal(hvt.results$compression_summary[["Percent_of_cells_below_Quantization_Error_Threshold"]],0)  
+  expect_equal(hvt.results$compression_summary[["percentOfCellsBelowQuantizationErrorThreshold"]],0)  
 })
